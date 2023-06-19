100c777526fb412f2659f728f45d04a0edaa1b19
SerhiiChynin <sergey.chinin@gmail.com>
2023-06-19 11:06:12 +0300

    console command

--- unit_1/app.js
+++ unit_1/app.js
@@ -4,3 +4,14 @@ console.log(a.length);
 for (let i = 0; i < a.length; i++){
     console.log(a[i]);
 }
+
+//D: - имя диска
+//cd - change directory войти в папку
+// cls - clean screen очистить экран
+// cd .. - выход на уровень выше
+// md - made directory создать папку
+// rd - удалить папку
+// cd ..\.. выйти на уровень выше и выйти на уровень выше
+// dir > text.txt - вывод содержимого в файл текст с разрешением тхт
+
+
