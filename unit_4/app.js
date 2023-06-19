d2c29d3ee76b8333fff1c040162a74d91a2bfa9f
SerhiiChynin <sergey.chinin@gmail.com>
2023-06-19 14:19:42 +0300

    express

---
+++ unit_4/app.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+let app = express();
+app.use(express.static('public')); // public - имя папки где хранится статика
+
+app.listen(3000, () => {
+    console.log('node express server');
+});
+
+app.get('/', (req, res) => {
+    console.log('load /');
+    res.render('index.html'); // render метод для загрузки файла
+})
+
+app.get('/cat', (req, res) => {
+    res.render('index2.html')
+})
