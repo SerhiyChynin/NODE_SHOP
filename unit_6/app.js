1c9c238124f4523eb80d9938de2df7c0ffdd272b
SerhiiChynin <sergey.chinin@gmail.com>
2023-06-20 12:40:38 +0300

    pug

--- unit_6/app.js
+++ unit_6/app.js
@@ -12,7 +12,29 @@ app.listen(3000, () => {
 });
 
 app.get('/', (req, res) => {
-    res.render('main');  // render метод для загрузки файла
+    res.render('main', {
+        foo: 4,
+        bar: 7,
+        tata: 'hello'
+    });  // render метод для загрузки файла
+})
+
+app.get('/sec', (req, res) => {
+    res.render('sec', {
+        one: 222,
+        two: 333,
+        three: 'https://cdn3.iconfinder.com/data/icons/ukraine-3/512/map_country_nation_ukraine_design_icons_free-256.png'
+    });  // render метод для загрузки файла
+})
+
+app.get('/shop', (req, res) => {
+    res.render('shop', {
+        one: 'https://cdn2.iconfinder.com/data/icons/logos-brands-4/24/Apple-256.png',
+        two: 'https://cdn2.iconfinder.com/data/icons/logos-brands-4/24/logo_brand_brands_logos_chrome-256.png',
+        three: 'https://cdn2.iconfinder.com/data/icons/logos-brands-4/24/logo_brand_brands_logos_microsoft_store_windows-256.png',
+        four: 'https://cdn2.iconfinder.com/data/icons/logos-brands-4/24/logo_brand_brands_logos_android-256.png',
+        five: 'https://cdn2.iconfinder.com/data/icons/logos-brands-4/24/logo_brand_brands_logos_playstore_google-256.png'
+    });  // render метод для загрузки файла
 })
 
 app.get('/cat', (req, res) => {
