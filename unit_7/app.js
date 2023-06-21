98bd18f8148bd295c507e108121a06719313c827
SerhiiChynin <sergey.chinin@gmail.com>
2023-06-21 16:32:48 +0300

    node_shop

--- unit_7/app.js
+++ unit_7/app.js
@@ -1,1 +1,36 @@
-// MySql
+
+const express = require('express');
+const app = express();
+app.set('view engine', 'pug');
+app.use(express.static('public')); // public - имя папки где хранится статика
+
+//подключаем модуль mySql
+const mysql = require('mysql');
+//настраиваем модуль
+let conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'node_shop'
+})
+
+app.listen(3000, () => {
+    console.log('node express server');
+});
+
+app.get('/', (req, res) => {
+    conn.query(
+        'SELECT * FROM goods',
+        function(err, res) {
+            if (err) throw err; //если ошибка бросаем исключение
+        }
+    );
+    res.render('main', {
+        foo: 4,
+        bar: 7,
+        tata: 'hello'
+    });  // render метод для загрузки файла
+})
+
+
+
