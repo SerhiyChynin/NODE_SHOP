9eb6986ca9fc04a2c0d842b7a49717ddd87d12e7
SerhiiChynin <sergey.chinin@gmail.com>
2023-06-27 11:59:58 +0300

    nav js + css

--- unit_8/public/js/nav.js
+++ unit_8/public/js/nav.js
@@ -1,10 +1,24 @@
 console.log('===========');
 
+document.querySelector('.close-nav').onclick = closeNav;
+document.querySelector('.show-nav').onclick = showNav;
+
+function showNav() {
+document.querySelector('.site-nav').style.left = '0';
+}
+
+function closeNav() {
+document.querySelector('.site-nav').style.left = '-300px';
+    
+
+}
+
 function getCategoryList() {
     fetch('/get-category-list', {
             method: 'POST'
         }
     ).then((response) => {
+        console.log(response);
             return response.text();
         }
     ).then((body) => {
@@ -17,5 +31,11 @@ function getCategoryList() {
 
 function showCategoryList(data) {
     console.log(data);
+    let out = '<ul class="category-list"><li><a href="/">Main</a></li>';
+    for (let i = 0; i < data.length; i++){
+        out += `<li><a href="/cat?id=${data[i].id}">${data[i].category}</a></li>`
+    }
+    out += '</ul>';
+    document.querySelector('#category-list').innerHTML = out;
 }
 getCategoryList();
