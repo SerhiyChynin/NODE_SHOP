d0c574983db087b96c32c50d12684dee07a90e8d
SerhiiChynin <sergey.chinin@gmail.com>
2023-06-28 13:02:44 +0300

    show goods cart

--- unit_8/public/js/card.js
+++ unit_8/public/js/card.js
@@ -26,9 +26,20 @@ function ajaxGetGoodsInfo() {
         } 
     })
     .then((res) => {
-        
+        return res.text();
     })
     .then((body) => {
         console.log(body);
+        showCart(JSON.parse(body))
     })
+}
+
+function showCart(data) {
+    let out = '<table class="table table-striped table-cart><tbody>';
+    let total = 0;
+    for (let key in cart){
+        out += `<tr><td><a href="goods?id=${key}">${data[key]['name']}</a></a></tr>`;
+    }
+    out += '</tbody></table>';
+    document.querySelector('#cart-nav').innerHTML = out;
 }
