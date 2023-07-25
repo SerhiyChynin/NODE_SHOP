b8b33fbf5bbbb2d74c5482991b6c450bf6c10d88
SerhiiChynin <sergey.chinin@gmail.com>
2023-07-25 12:43:53 +0300

    admin panel order+user

--- unit_8/app.js
+++ unit_8/app.js
@@ -156,8 +156,25 @@ app.get('/admin', function (req, res) {
   res.render('admin', {})
 });
 app.get('/admin-order', function (req, res) {
-  con.query('SELECT * FROM shop_order ORDER BY date DESC', function (error, result, fields) {
+  con.query(`SELECT 
+	shop_order.id as id,
+	shop_order.user_id as user_id,
+    shop_order.goods_id as goods_id,
+    shop_order.goods_cost as goods_cost,
+    shop_order.goods_amount as goods_amount,
+    shop_order.total as total,
+    from_unixtime(date, "%y-%m-%d %h:%m") as human_date,
+    user_info.user_name as user,
+    user_info.user_phone as phone,
+    user_info.address as address
+FROM
+	shop.shop_order
+LEFT JOIN
+	user_info
+ON  shop_order.user_id = user_info.id;
+`, function (error, result, fields) {
     if (error) throw error;
+    console.log(result);
     res.render('admin-order', { order: JSON.parse(JSON.stringify(result)) });
   });
 });
